cc3c802d1f5412be75a7d216216410e111edf0da
unknown <ward.alexander@outlook.com>
2023-03-14 10:00:21 -0700

    Initial commit

--- script/password.js
+++ script/password.js
@@ -1,11 +1,11 @@
 function passwordCheck(){
     var password = prompt("Please enter the password.");
     if (password==="community"){
-        window.location="./pages/services.html";
+        window.location="./pages/linktree.html";
     } else{
         while(password !=="community"){
             password = prompt("Please enter the password.");
         }
-        window.location="./pages/services.html";
+        window.location="./pages/linktree.html";
     }
 }
